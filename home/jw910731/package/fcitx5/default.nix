009d39d0245df87285a6c4b30a89b9fab0d97470
jw910731 <jw910731@gmail.com>
2024-01-19 00:51:06 +0800

    Fine tune fcitx

---
+++ home/jw910731/package/fcitx5/default.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, ... }:
+{
+  home.file.".local/share/fcitx5/rime/default.custom.yaml".source=./default.custom.yaml;
+
+  i18n.inputMethod = {
+    enabled = "fcitx5";
+    fcitx5.addons = with pkgs; [
+      fcitx5-rime
+      fcitx5-configtool
+    ];
+  };
+
+  xdg.configFile = {
+    "fcitx5/profile" = {
+      source = ./profile;
+      # every time fcitx5 switch input method, it will modify ~/.config/fcitx5/profile,
+      # so we need to force replace it in every rebuild to avoid file conflict.
+      force = true;
+    };
+    "fcitx5/conf/classicui.conf".source = ./classicui.conf;
+  };
+
+  
+}
