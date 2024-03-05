3437eb11d94ffbff35a06c9e8f09677e5b061ea7
jw910731 <jw910731@gmail.com>
2024-03-05 22:36:03 +0800

    Add AMD proprietary driver

--- system/linux-host/linux-host.nix
+++ system/linux-host/linux-host.nix
@@ -4,6 +4,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxKernel.packages.linux_zen;
+  boot.extraModulePackages = [ config.boot.kernelPackages.amdgpu-pro ];
   boot.initrd.kernelModules = [ "amdgpu" ];
   boot.loader.systemd-boot.configurationLimit = 10;
 
