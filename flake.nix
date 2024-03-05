a0c2a03626d08a7a5336a88aeff2aafa17cb6bc6
jw910731 <jw910731@gmail.com>
2024-03-05 21:19:52 +0800

    Remove home manager from tree

--- flake.nix
+++ flake.nix
@@ -22,15 +22,6 @@
         system = "x86_64-linux";
         modules = [
           ./system/linux-host/configuration.nix
-
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-
-            home-manager.users = import ./home;
-            home-manager.extraSpecialArgs = inputs;
-          }
         ];
       };
     };
