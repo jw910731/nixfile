5dd5901261c65399d5481d5b6de8ac26b47534bf
jw910731 <jw910731@gmail.com>
2024-04-08 21:23:40 +0800

    Change ssh port to deploy git server on host

--- system/linux-host/linux-host-network.nix
+++ system/linux-host/linux-host-network.nix
@@ -13,9 +13,18 @@
   networking.firewall = {
     enable = true;
     trustedInterfaces = ["wlp5s0"];
-    allowedTCPPorts = [9345 6443 80 443];
-    extraCommands="iptables -A nixos-fw -p all -s 192.168.0.0/24 -j nixos-fw-accept";
+    allowedTCPPorts = [9345 6443 80 443 22];
+    extraCommands=''
+      iptables -A nixos-fw -p all -s 192.168.0.0/24 -j nixos-fw-accept
+    '';
   };
+
+  services.openssh.listenAddresses = [
+    {
+      addr = "0.0.0.0";
+      port = 10022;
+    }
+  ];
 
   networking.networkmanager.unmanaged = ["cali*" "flannel*"];
 
