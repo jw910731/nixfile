169213c45fd976a5c71566843e90a078f3261356
jw910731 <jw910731@gmail.com>
2024-05-01 03:32:13 +0800

    Install emacs extra pkg and related dependency

--- home/jw910731/common/package/emacs.nix
+++ home/jw910731/common/package/emacs.nix
@@ -3,5 +3,13 @@
   programs.emacs = {
     enable = true;
     package = pkgs.emacs-nox;
+    extraPackages = epkgs: [
+      epkgs.vterm
+    ];
   };
+  home.packages = with pkgs; [
+    ripgrep
+    coreutils # basic GNU utilities
+    fd
+  ];
 }
