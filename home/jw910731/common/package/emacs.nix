b5747ca9ba90c85465c661171509cc08546f60bf
jw910731 <jw910731@gmail.com>
2024-05-01 22:38:16 +0800

    Add doomemacs config

--- home/jw910731/common/package/emacs.nix
+++ home/jw910731/common/package/emacs.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, config, ... }:
 {
   programs.emacs = {
     enable = true;
@@ -12,4 +12,12 @@
     coreutils # basic GNU utilities
     fd
   ];
+
+  home.activation.install-doom = lib.hm.dag.entryAfter [ "installPackages" ] ''
+    if ! [ -d "${config.xdg.configHome}/emacs" ]; then
+      $DRY_RUN_CMD ${lib.getExe pkgs.git} clone $VERBOSE_ARG --depth=1 --single-branch "https://github.com/doomemacs/doomemacs.git" "${config.xdg.configHome}/emacs"
+    fi
+  '';
+
+  xdg.configFile."doom".source = ./doom ;
 }
