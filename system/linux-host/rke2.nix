1c14f5e75f5feb0902da23b093b27a2afb91d0a1
jw910731 <jw910731@gmail.com>
2024-06-28 04:20:37 +0800

    Upgrade rke2 to 1.28.10

--- system/linux-host/rke2.nix
+++ system/linux-host/rke2.nix
@@ -1,11 +1,11 @@
 final: prev:
 let
-  go_version = "1.21.7";
+  go_version = "1.21.9";
   go = prev.go_1_21.overrideAttrs rec {
     version = go_version;
     src = prev.fetchurl {
       url = "https://go.dev/dl/go${version}.src.tar.gz";
-      hash = "sha256-ABl6sg8zgTgyv/Yv2TzKHEKgjMaJoypmcspJWRlZv/Y=";
+      hash = "sha256-WPDFztRaABK84v96nfA+Eoq8yIGOur5QJ7uSuv4g5CE=";
     };
   };
   buildGoModule = prev.buildGo121Module.override {
@@ -17,17 +17,17 @@ in
   rke2 = prev.rke2.override {
     buildGoModule = args: buildGoModule (args // rec {
       nativeBuildInputs = [ prev.yq-go prev.curl prev.git ];
-      version = "1.27.11+rke2r1";
+      version = "1.28.10+rke2r1";
 
       src = prev.fetchFromGitHub {
         owner = "rancher";
         repo = args.pname;
         rev = "v${version}";
-        hash = "sha256-tCX/fatYqG/TLd33oY4+XkEL3L9L2RqZNteMNN7XxmE=";
+        hash = "sha256-hPAmsXf+06tgiV/qtfZcIS+meAIyN7ST81t2/T4Rd/Q=";
         leaveDotGit = true;
       };
 
-      vendorHash = "sha256-elK0gPZmCW7kfGmnXmkJ2C6GFGMb9jn4Hla47PeqUJI=";
+      vendorHash = "sha256-iidkTSrrHyW5ZEouzHAWUwCC9nplGz1v/E9bM2lMPeM=";
 
       postPatch = ''
         # Patch the build scripts so they work in the Nix build environment.
