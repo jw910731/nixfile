7a055f7b7cc303aea4023e3f4e14d6b9ff72c17c
jw910731 <jw910731@gmail.com>
2024-10-22 23:41:22 +0800

    Add devenv removal script

--- home/jw910731/common/package/zsh/default.nix
+++ home/jw910731/common/package/zsh/default.nix
@@ -120,8 +120,8 @@ in
 
   home.file.".p10k".source = ./p10k;
   home.file.".shell".source = ./shell;
-  home.file."dev/bin/remote-viewer" = {
-    source = ./dev/bin/remote-viewer;
+  home.file."dev/bin/rmdv" = {
+    source = ./dev/bin/rmdv;
     executable = true;
   };
 }
