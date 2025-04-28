b8edf510f756235adb9557476909234b3edf22ec
jw910731 <jw910731@gmail.com>
2025-04-28 21:46:01 +0800

    Remove iterm integration

--- home/jw910731/darwin/zsh.nix
+++ home/jw910731/darwin/zsh.nix
@@ -11,15 +11,6 @@
           sha256 = "sha256-x+6UPghRB64nxuhJcBaPQ1kPhsDx3HJv0TLJT5rjZpA=";
         };
       }
-      {
-        name = "iterm2-integration";
-        file = "";
-        src = pkgs.fetchurl {
-          name = "iterm2_shell_integration.zsh";
-          url = "https://iterm2.com/shell_integration/zsh";
-          sha256 = "sha256-Cq8winA/tcnnVblDTW2n1k/olN3DONEfXrzYNkufZvY=";
-        };
-      }
     ];
   };
 }
