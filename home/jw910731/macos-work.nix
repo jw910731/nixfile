4a9ff9adb1a3ca93825781be07274624dc180701
jw910731 <jw910731@gmail.com>
2025-05-22 14:19:48 +0800

    Use internal tool to manage git config

--- home/jw910731/macos-work.nix
+++ home/jw910731/macos-work.nix
@@ -7,18 +7,9 @@
   ];
   home.username = "jw910731";
   home.homeDirectory = lib.mkForce "/Users/jw910731";
-  programs.git = {
-    userName = "Jerry Wu";
-    userEmail = "jw910731@apple.com";
-    # Sign
-    signing = {
-      signByDefault = true;
-      key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPUeTUoxHOR/pJkviU2gAgUROvM5rZb5d73ud7HML8B+";
-    };
-    extraConfig.gpg = {
-      format = "ssh";
-    };
-  };
+  
+  # Auto managed by internal tool
+  programs.git.enable = lib.mkForce false;
 
   home.packages = with pkgs; [
   ];
