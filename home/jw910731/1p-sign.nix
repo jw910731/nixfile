129b68f21c7c7ab32221c47cdfa4f7e04044f07e
jw910731 <jw910731@gmail.com>
2025-06-01 18:00:25 +0800

    Try fix SSH_AUTH_SOCK on linux

--- home/jw910731/1p-sign.nix
+++ home/jw910731/1p-sign.nix
@@ -16,6 +16,6 @@
       if pkgs.stdenv.isDarwin then
         "${config.home.homeDirectory}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
       else
-        "";
+        null;
   };
 }
