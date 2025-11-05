a86eeb26737b176b16ab9876887b1704711b1cc9
jw910731 <jw910731@gmail.com>
2025-11-06 06:42:34 +0800

    Upgrade asahi nix

--- system/asahi/asahi.nix
+++ system/asahi/asahi.nix
@@ -21,6 +21,15 @@
     };
 
     settings.auto-optimise-store = true;
+
+    settings = {
+      extra-substituters = [
+        "https://nixos-apple-silicon.cachix.org"
+      ];
+      extra-trusted-public-keys = [
+        "nixos-apple-silicon.cachix.org-1:8psDu5SA5dAD7qA0zMy5UT292TxeEPzIz8VVEr2Js20="
+      ];
+    };
   };
 
   # $ nix search wget
