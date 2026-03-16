22d781350ccfa205e0a7412f48104e7be72aada4
jw910731 <jw910731@gmail.com>
2026-03-16 15:01:23 +0800

    Enlarge ghostty scrollback buffer size

--- home/jw910731/common/package/ghostty.nix
+++ home/jw910731/common/package/ghostty.nix
@@ -11,7 +11,7 @@
       font-size = 14;
       font-thicken = true;
       faint-opacity = 0.75;
-      scrollback-limit = 20000;
+      scrollback-limit = 104857600;
       shell-integration-features = "no-cursor,sudo,title,ssh-env";
       app-notifications =  "no-clipboard-copy";
       cursor-style = "block";
