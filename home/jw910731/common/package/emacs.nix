75c4ebbae0c6ac3728a79dc0eed8d7ca1ac6da46
jw910731 <jw910731@gmail.com>
2026-04-12 01:04:07 +0800

    Use fetch tree for doom emacs

--- home/jw910731/common/package/emacs.nix
+++ home/jw910731/common/package/emacs.nix
@@ -9,6 +9,7 @@
 
   programs.doom-emacs = {
     enable = true;
+    experimentalFetchTree = true; # Disable me when doom-emacs failed while fetching
     emacs = pkgs.emacs-nox;
     extraPackages = epkgs: [ epkgs.vterm ];
     doomDir = ./doom; # or e.g. `./doom.d` for a local configuration
