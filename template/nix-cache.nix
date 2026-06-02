b89ede3ccac2891cb3753e3cef55ded3211083f6
jw910731 <jw910731@gmail.com>
2026-06-02 22:35:40 +0800

    Remove garnix in cache
    Since garnix is about to EOL

--- template/nix-cache.nix
+++ template/nix-cache.nix
@@ -1,6 +1,5 @@
 let
   caches = [
-    "https://cache.garnix.io"
     "https://nixos-apple-silicon.cachix.org"
     "https://claude-code.cachix.org"
   ];
@@ -10,7 +9,6 @@ in
     substituters = caches;
     trusted-substituters = caches;
     trusted-public-keys = [
-      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       "nixos-apple-silicon.cachix.org-1:8psDu5SA5dAD7qA0zMy5UT292TxeEPzIz8VVEr2Js20="
       "claude-code.cachix.org-1:YeXf2aNu7UTX8Vwrze0za1WEDS+4DuI2kVeWEE4fsRk="
     ];
