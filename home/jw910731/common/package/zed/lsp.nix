c8e409ae183237744efa9052f3e2ad9def6fe2be
jw910731 <jw910731@gmail.com>
2026-07-11 10:21:34 +0800

    Fix clangd in Zed query driver

--- home/jw910731/common/package/zed/lsp.nix
+++ home/jw910731/common/package/zed/lsp.nix
@@ -1,3 +1,12 @@
 {
-  
+  clangd.binary.arguments = [
+    "--query-driver"
+    "/nix/store/*/bin/*gcc"
+    "--query-driver"
+    "/nix/store/*/bin/*clang"
+    "--query-driver"
+    "/nix/store/*/bin/*g++"
+    "--query-driver"
+    "/nix/store/*/bin/*clang++"
+  ];
 }
