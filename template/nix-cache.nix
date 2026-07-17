55cde7afeb76b19e2a41e1359c5452ddf4e0711d
jw910731 <jw910731@gmail.com>
2026-07-17 19:45:01 +0800

    Use numtide cache for llm-agents flake

--- template/nix-cache.nix
+++ template/nix-cache.nix
@@ -1,7 +1,7 @@
 let
   caches = [
     "https://nixos-apple-silicon.cachix.org"
-    "https://claude-code.cachix.org"
+    "https://cache.numtide.com"
   ];
 in
 {
@@ -10,7 +10,7 @@ in
     trusted-substituters = caches;
     trusted-public-keys = [
       "nixos-apple-silicon.cachix.org-1:8psDu5SA5dAD7qA0zMy5UT292TxeEPzIz8VVEr2Js20="
-      "claude-code.cachix.org-1:YeXf2aNu7UTX8Vwrze0za1WEDS+4DuI2kVeWEE4fsRk="
+      "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
     ];
   };
 }
