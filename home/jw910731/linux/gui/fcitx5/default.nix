8804cc99e14f48953ba9957fedbc5072f12f8936
jw910731 <jw910731@gmail.com>
2026-08-14 11:38:56 +0800

    Change RIME keybind to mimic macOS IME

--- home/jw910731/linux/gui/fcitx5/default.nix
+++ home/jw910731/linux/gui/fcitx5/default.nix
@@ -31,4 +31,11 @@
     "fcitx5/conf/classicui.conf".source = ./classicui.conf;
   };
 
+  # nix store 檔案 mtime 恆為 epoch，librime 以 mtime 判斷 custom 檔是否變更，
+  # 會永遠認為沒變而不重新編譯；每次 switch 強制作廢編譯快取（重啟 fcitx5 後生效）
+  home.activation.rimeForceRecompile = config.lib.dag.entryAfter [ "writeBoundary" ] ''
+    $DRY_RUN_CMD rm -f "$HOME/.local/share/fcitx5/rime/build/default.yaml" \
+      "$HOME/.local/share/fcitx5/rime/build/"*.schema.yaml
+  '';
+
 }
