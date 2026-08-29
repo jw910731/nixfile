72348d190c285e8c8198ec4fccd454ae558c054d
jw910731 <jw910731@gmail.com>
2026-08-30 01:57:43 +0800

    Fix ssh common config permission error

--- home/jw910731/common/ssh.nix
+++ home/jw910731/common/ssh.nix
@@ -1,11 +1,19 @@
 { pkgs, lib, ... }:
 {
-  home.file.".ssh/common.config".text = ''
-    Host *
-        ${lib.optionalString pkgs.stdenv.isDarwin ''IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"''}
-        ForwardAgent yes
-        ControlMaster auto
-        ControlPath ~/.ssh/ssh_mux_%h_%p_%r
-        Compression yes
-  '';
+  home.file.".ssh/common.config_link" = {
+      text = ''
+      Host *
+          ${lib.optionalString pkgs.stdenv.isDarwin ''IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"''}
+          ForwardAgent yes
+          ControlMaster auto
+          ControlPath ~/.ssh/ssh_mux_%h_%p_%r
+          Compression yes
+    '';
+    onChange = ''
+      cat ~/.ssh/common.config_link > ~/.ssh/common.config
+      rm ~/.ssh/.ssh/common.config_link
+      chmod 600 ~/.ssh/common.config
+    '';
+    force = true;
+  };
 }
