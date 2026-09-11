9b7488b2bfd3d855abb4f6ef8ded93b85b1a85b0
jw910731 <jw910731@gmail.com>
2026-09-11 23:12:14 +0800

    Refactor linux config to common base and server/desktop scenario templates

--- template/linux/default.nix
+++ template/linux/default.nix
@@ -1,92 +1,144 @@
-{ pkgs, ... }:
 {
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  options.jw.secureBoot = lib.mkOption {
+    type = lib.types.bool;
+    default = false;
+    description = "Enable secure boot (lanzaboote is wired per-machine where its module is imported)";
+  };
+
   imports = [
     ../nix-cache.nix
   ];
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
+  config = lib.mkMerge [
+    {
+      # Select internationalisation properties.
+      i18n.defaultLocale = "en_US.UTF-8";
 
-  i18n.supportedLocales = [
-    "en_US.UTF-8/UTF-8"
-    "zh_TW.UTF-8/UTF-8"
-  ];
+      i18n.extraLocaleSettings = {
+        LC_ADDRESS = "en_US.UTF-8";
+        LC_IDENTIFICATION = "en_US.UTF-8";
+        LC_MEASUREMENT = "en_US.UTF-8";
+        LC_MONETARY = "en_US.UTF-8";
+        LC_NAME = "en_US.UTF-8";
+        LC_NUMERIC = "en_US.UTF-8";
+        LC_PAPER = "en_US.UTF-8";
+        LC_TELEPHONE = "en_US.UTF-8";
+        LC_TIME = "en_US.UTF-8";
+      };
 
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
-
-  # Nix settings
-  nixpkgs.config.allowUnfree = true;
-
-  nix = {
-    package = pkgs.lix;
-
-    settings = {
-      auto-optimise-store = true;
-      experimental-features = [
-        "nix-command"
-        "flakes"
+      i18n.supportedLocales = [
+        "en_US.UTF-8/UTF-8"
+        "zh_TW.UTF-8/UTF-8"
       ];
-      trusted-users = [
-        "@wheel"
+
+      # Configure keymap in X11
+      services.xserver.xkb = {
+        layout = "us";
+        variant = "";
+      };
+
+      # Nix settings
+      nixpkgs.config.allowUnfree = true;
+
+      nix = {
+        package = pkgs.lix;
+
+        settings = {
+          auto-optimise-store = true;
+          experimental-features = [
+            "nix-command"
+            "flakes"
+          ];
+          trusted-users = [
+            "@wheel"
+          ];
+        };
+
+        gc = {
+          automatic = true;
+          dates = "weekly";
+          options = "--delete-older-than 14d";
+        };
+      };
+
+      # Some minimal needed packages
+      environment.systemPackages = with pkgs; [
+        git
+        vim
+        joe
+        zsh
+        man-pages
+        man-pages-posix
+        strace
+        gcc
+        gnumake
+        tmux
+        killall
+        cifs-utils
       ];
-    };
 
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 14d";
-    };
-  };
+      # basic open ssh
+      services.openssh = {
+        enable = true;
+        settings.PermitRootLogin = "no";
+        settings.StreamLocalBindUnlink = "yes";
+        settings.PasswordAuthentication = false;
+        settings.KbdInteractiveAuthentication = false;
+      };
 
-  # Some minimal needed packages
-  environment.systemPackages = with pkgs; [
-    git
-    vim
-    joe
-    zsh
-    man-pages
-    man-pages-posix
-    strace
-    gcc
-    gnumake
-    tmux
-    killall
+      networking.networkmanager.enable = true;
+      networking.firewall.enable = true;
+
+      services.avahi = {
+        enable = true;
+        nssmdns4 = true;
+        openFirewall = true;
+        publish = {
+          enable = true;
+          userServices = true;
+          addresses = true;
+        };
+      };
+
+      time.timeZone = "Asia/Taipei";
+
+      users.users.jw910731 = {
+        isNormalUser = lib.mkDefault true;
+        description = lib.mkDefault "Jerry Wu";
+        shell = lib.mkDefault pkgs.zsh;
+        extraGroups = [
+          "networkmanager"
+          "wheel"
+          "docker"
+        ];
+      };
+
+      boot.loader.efi.canTouchEfiVariables = lib.mkDefault true;
+      boot.initrd.kernelModules = lib.mkDefault [ ];
+      boot.loader.systemd-boot.enable = lib.mkDefault (!config.jw.secureBoot);
+      # seen as mandatory config
+      documentation.dev.enable = true;
+      programs.dconf.enable = true;
+      fonts.fontDir.enable = true;
+
+      # Make vscode server runnable
+      programs.nix-ld.enable = true;
+
+      # 1password cli
+      programs._1password.enable = true;
+
+      # Enable zsh
+      programs.zsh.enable = true;
+      environment.pathsToLink = [ "/share/zsh" ];
+    }
+    {
+      environment.systemPackages = lib.mkIf config.jw.secureBoot [ pkgs.sbctl ];
+    }
   ];
-
-  # basic open ssh
-  services.openssh = {
-    enable = true;
-    settings.PermitRootLogin = "no";
-    settings.StreamLocalBindUnlink = "yes";
-  };
-
-  # seen as mandatory config
-  documentation.dev.enable = true;
-  programs.dconf.enable = true;
-  fonts.fontDir.enable = true;
-
-  # Make vscode server runnable
-  programs.nix-ld.enable = true;
-
-  # 1password cli
-  programs._1password.enable = true;
-
-  # Enable zsh
-  programs.zsh.enable = true;
-  environment.pathsToLink = [ "/share/zsh" ];
 }
---
+++ template/linux/desktop.nix
@@ -0,0 +1,169 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  options.jw.fingerprint = lib.mkOption {
+    type = lib.types.bool;
+    default = false;
+    description = "Enable fingerprint sensor + lid handling";
+  };
+  config = lib.mkMerge [
+    {
+      networking.firewall.allowedTCPPorts = [ ];
+
+      services.displayManager.sddm = {
+        enable = true;
+        wayland.enable = true;
+      };
+      services.desktopManager.plasma6.enable = true;
+
+      xdg.portal = {
+        enable = true;
+        extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
+      };
+      programs.hyprland = {
+        enable = true;
+        withUWSM = true;
+        xwayland.enable = true;
+      };
+      programs.uwsm.enable = true;
+      environment.sessionVariables.NIXOS_OZONE_WL = "1";
+      environment.etc."xdg/menus/applications.menu".source =
+        "${pkgs.kdePackages.plasma-workspace}/etc/xdg/menus/plasma-applications.menu";
+      security.polkit.enable = true;
+
+      services.thermald.enable = true;
+      powerManagement = {
+        enable = true;
+        powertop.enable = true;
+      };
+      services.logind.settings.Login.HandlePowerKey = "ignore";
+
+      boot.kernelModules = [ "i2c-dev" ];
+      hardware.i2c.enable = true;
+
+      # Bluetooth
+      hardware.bluetooth = {
+        enable = true;
+        powerOnBoot = true;
+      };
+      services.blueman.enable = true;
+
+      # Disable CUPS for print documents.
+      services.printing.enable = false;
+
+      # Enable flatpak
+      services.flatpak.enable = true;
+
+      hardware.enableAllFirmware = true;
+
+      # Local Send (Airdrop alternative)
+      programs.localsend = {
+        enable = true;
+        package = derivation {
+          name = "empty";
+          builder = "${pkgs.uutils-coreutils-noprefix}/bin/mkdir";
+          args = [ "${builtins.placeholder "out"}" ];
+          system = pkgs.system;
+        };
+        openFirewall = true;
+      };
+
+      environment.systemPackages = with pkgs; [
+        kdePackages.kio
+        kdePackages.kio-fuse
+        kdePackages.kio-extras
+        ddcutil
+      ];
+
+      # Enable sound with pipewire.
+      services.pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+        jack.enable = true;
+
+        # Bluetooth
+        wireplumber = {
+          enable = true;
+          extraConfig."10-bluez" = {
+            "monitor.bluez.properties" = {
+              "bluez5.enable-sbc-xq" = true;
+              "bluez5.enable-msbc" = true;
+              "bluez5.enable-hw-volume" = true;
+              "bluez5.roles" = [
+                "hsp_hs"
+                "hsp_ag"
+                "hfp_hf"
+                "hfp_ag"
+                "a2dp_sink"
+                "a2dp_source"
+                "bap_sink"
+                "bap_source"
+              ];
+              "bluez5.codecs" = [
+                "ldac"
+                "aptx"
+                "aptx_ll_duplex"
+                "aptx_ll"
+                "aptx_hd"
+                "opus_05_pro"
+                "opus_05_71"
+                "opus_05_51"
+                "opus_05"
+                "opus_05_duplex"
+                "aac"
+                "sbc_xq"
+              ];
+
+              "bluez5.hfphsp-backend" = "none";
+            };
+          };
+        };
+
+        # Airplay (Disabled for now)
+        raopOpenFirewall = false;
+        # extraConfig.pipewire = {
+        #   "10-airplay" = {
+        #     "context.modules" = [
+        #       {
+        #         name = "libpipewire-module-raop-discover";
+        #       }
+        #     ];
+        #   };
+        # };
+      };
+
+      # 1Password requires a polkit policy owner.
+      programs._1password-gui = {
+        enable = true;
+        polkitPolicyOwners = [ "jw910731" ];
+      };
+    }
+    (lib.mkIf config.jw.fingerprint {
+      security.pam.services.login.fprintAuth = false;
+
+      # Fingerprint sensor
+      services.fprintd.enable = true;
+      services.acpid = {
+        enable = true;
+        lidEventCommands =
+          let
+            goodixVendor = "27c6";
+          in
+          ''
+            grep -q closed /proc/acpi/button/lid/LID0/state
+            if [ $? = 0 ]; then
+              ${pkgs.fd}/bin/fd "-" /sys/bus/usb/devices --exec /bin/sh -c '${pkgs.gnugrep}/bin/grep -qs ${goodixVendor} {}/idVendor && ${pkgs.coreutils}/bin/echo 0 > {}/authorized'
+            else
+              ${pkgs.fd}/bin/fd "-" /sys/bus/usb/devices --exec /bin/sh -c '${pkgs.gnugrep}/bin/grep -qs ${goodixVendor} {}/idVendor && ${pkgs.coreutils}/bin/echo 1 > {}/authorized'
+            fi
+          '';
+      };
+    })
+  ];
+}
